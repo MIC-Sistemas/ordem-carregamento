ca065da274a0547ab355bbc912a18a9462213a39
Thiago Valerio Pires <thiago.valerio@gmail.com>
2025-04-09 19:38:40 -0400

    spring base

---
+++ src/main/java/br/com/micsistemas/ordem_carregamento/OrdemCarregamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.micsistemas.ordem_carregamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdemCarregamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdemCarregamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/micsistemas/ordem_carregamento/OrdemCarregamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.micsistemas.ordem_carregamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdemCarregamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
